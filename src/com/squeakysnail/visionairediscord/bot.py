3947f1a2b971c42fbeb29b77dd506f9729590b78
Ong Beng Tiong <ongbt@i2r.a-star.edu.sg>
2017-08-06 17:05:28 +0800

    Modified bot.py:  - added command !sold to show last unit sold  - added command !cache! to invalidate the cache

--- src/com/squeakysnail/visionairediscord/bot.py
+++ src/com/squeakysnail/visionairediscord/bot.py
@@ -20,7 +20,8 @@ unitCount = ""
 checkInterval = 8
 client = discord.Client()
 topDate = datetime.datetime(2018, 10, 31, 0,0,0)
-
+data = ""
+lastSoldUnit = ""
       
 @client.event
 async def on_ready():
@@ -32,15 +33,17 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global lastCheckedTime
-    global unitCount   
+    global unitCount  
+    global lastSoldUnit 
+    global data
     currentTime = datetime.datetime.now()
     timeDelta = currentTime - lastCheckedTime  
     hours, remainder = divmod(timeDelta.seconds, 3600)
     if message.content == '!help':
-        await client.send_message(message.channel, "!units: units left.\n!top: time to TOP")
+        await client.send_message(message.channel, "!units:\t\tUnits Left.\n!top:\t\tTime to TOP\n!sold:\t\tLast Sold Unit.")
     if message.content == '!v' or message.content == '!units':
         isCached = "!"
-        if hours>checkInterval or unitCount=="":
+        if hours>checkInterval or data=="":
             lastCheckedTime = datetime.datetime.now()
             await client.send_message(message.channel, 'Checking Visionaire units...')
             r  = requests.get(url)
@@ -50,14 +53,29 @@ async def on_message(message):
                 unitCount = link 
                 isCached = ""
         await client.send_message(message.channel, unitCount + isCached)
-    if message.content == '!cache':
+    if message.content == '!sold':
+        if hours>checkInterval or data=="":
+            lastCheckedTime = datetime.datetime.now()
+            await client.send_message(message.channel, 'Checking Visionaire units...')
+            r  = requests.get(url)
+            data = r.text
+            soup = BeautifulSoup(data)
+            for link in soup.find_all(text=re.compile("Recently SOLD units:")):
+                lastSoldUnit = link.parent.parent.nextSibling.nextSibling.nextSibling + ""
+        await client.send_message(message.channel, lastSoldUnit)
+    if message.content.startswith('!cache'):
         #timeToNextCheck = lastCheckedTime -timeDelta + checkInterval
         await client.send_message(message.channel, "Last checked: "+ str(lastCheckedTime) +"]" + " ["+ str(checkInterval-hours) +" hours before next check.]")
+        if message.content == '!cache!':
+            data = ""
+            await client.send_message(message.channel, "Cache invalidated")
+        
     if message.content == '!top':
         timeToTop = topDate - currentTime
         await client.send_message(message.channel, "Time to TOP: "+ str(timeToTop.days) +" days!\nEstimated TOP: " + str(topDate))
          
             
+
 
 
 #client.run('MzQzMjQ0MDA0MDU5NjQzOTA2.DGgAOA.p41VGt3_wqKfkkD-acv432i2nIE')
